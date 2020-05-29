718c97e8d426b8af108c94417f0d13beaa8a4e7a
MTS <suzuki.motofumi@gmail.com>
2020-05-29 11:10:54 +0900

    Revised

--- ch07/q7-1.c
+++ ch07/q7-1.c
@@ -1,19 +1,18 @@
-/* code: q7-1.c   (v1.16.00) */
+/* code: q7-1.c   (v1.16.01) */
 #include <stdio.h>
 #include <stdlib.h>
 #include <search.h>
 
 #define ARRAY_SIZE 10
+#define NEW_ITEMS  1
 
 /* ------------------------------------------- */
-int compare (int *x, int *y)
-{
+int compare (int *x, int *y) {
   return (*x - *y);
 }
 
 /* ------------------------------------------- */
-void print_array (int array[], int n)
-{
+void print_array (int array[], int n) {
   int i;
   for (i = 0; i < n; i++) {
     printf ("%d ", array[i]);
@@ -22,8 +21,54 @@ void print_array (int array[], int n)
 }
 
 /* ------------------------------------------- */
-int main ()
-{
+int main () {
+  int key;
+  int *r;
+  size_t elements, elements_t;
+  int array[ARRAY_SIZE + NEW_ITEMS] = {
+    12, 19, 70, 44, 16, 38, 10, 30, 28, 98
+  };
+
+  key = 17;
+
+  elements_t = elements = ARRAY_SIZE;
+  print_array (array, ARRAY_SIZE);
+
+  printf ("elements: %zu\n", elements);
+
+  r = (int *) lsearch (&key, &array, &elements, sizeof (int),
+		       (int (*)(const void *, const void *)) compare);
+
+  if (r == NULL) {
+    printf ("ERROR: lsearch()");
+    exit (-1);
+  }
+
+  printf ("elements: %zu\n", elements);
+
+  if (elements == elements_t) {
+    printf ("Found : %d\n", key);
+  }
+  else {
+    printf ("Not found %d\n", key);
+    printf ("The item added at the end of the array.\n");
+    print_array (array, ARRAY_SIZE + NEW_ITEMS);
+  }
+
+  return 0;
+}
+
+/* 
+   The lsearch and lfind functions return  
+   a pointer to the first element found. 
+   If no element was found, lsearch returns 
+   a pointer to the newly added element, 
+   whereas lfind returns NULL. 
+*/
+
+/* ------------------------------------------- 
+
+int main () {
   int key;
   int *r;
   size_t elements;
@@ -34,9 +79,9 @@ int main ()
   elements = ARRAY_SIZE;
   print_array (array, ARRAY_SIZE);
 
-  r = (int *) lsearch (&key, &array, &elements, sizeof (int),
+  r = (int *) lfind (&key, &array, &elements, sizeof (int),
 		       (int (*)(const void *, const void *)) compare);
-  if (r != NULL) {
+  if ( r != NULL ) {
     printf ("Found: %d\n", key);
   }
   else {
@@ -44,3 +89,5 @@ int main ()
   }
   return 0;
 }
+
+  ------------------------------------------- */
